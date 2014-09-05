6140a179439eb68008c3d19d6e650111cedcb723
Markus Feng <zzz1112222595@hotmail.com>
2014-09-04 22:11:39 -0400

    Added sample web connection code

--- src/com/github/assisstion/InternetSpeedTest/MainGUI.java
+++ src/com/github/assisstion/InternetSpeedTest/MainGUI.java
@@ -1,7 +1,27 @@
 package com.github.assisstion.InternetSpeedTest;
 
+import java.io.IOException;
+import java.net.URL;
+
+import com.github.assisstion.InternetSpeedTest.web.WebConnector;
+
 public class MainGUI {
 	public static void main(String[] args){
+		System.out.println("Hello, world!");
+		connect();
 		System.out.println("Goodbye, world!");
+	}
+	
+	public static void connect(){
+		String urlString = "http://www.apple.com";
+		System.out.println("Connecting to page: " + urlString);
+		try {
+			URL url = new URL(urlString);
+			String page = WebConnector.getWebpage(url);
+			System.out.println(page);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/com/github/assisstion/InternetSpeedTest/web/WebConnector.java
@@ -0,0 +1,34 @@
+package com.github.assisstion.InternetSpeedTest.web;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class WebConnector {
+	
+	
+	/**
+	 * Gets the HTML representation of a webpage as a String
+	 * @param url the URL of the webpage
+	 * @return the HTML representation of the webpate
+	 * @throws IOException when the webpage cannot be connected to
+	 */
+	public static String getWebpage(URL url) throws IOException{
+		//Gets the input stream to load the website
+		InputStream is = url.openConnection().getInputStream();
+		//Wraps the input stream with a buffer
+		BufferedInputStream bis = new BufferedInputStream(is);
+		//Gives the int value of one unicode character
+		int value = bis.read();
+		String output = "";
+		while(value >= 0){
+			//Adds the unicode character to the output
+			output += (char) value;
+			//Sets the value to the next character
+			value = bis.read();
+		}
+		//Returns the webpage's html contents
+		return output;
+	}
+}
