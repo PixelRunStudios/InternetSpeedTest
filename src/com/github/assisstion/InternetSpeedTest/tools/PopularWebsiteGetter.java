4b1842d9499e76911cd2ff44718e75c0dca518bc
Alexander Guo <alexanderguo99@gmail.com>
2014-09-05 18:23:47 -0400

    Sorted name and url
    only linked websites

--- src/com/github/assisstion/InternetSpeedTest/tools/PopularWebsiteGetter.java
+++ src/com/github/assisstion/InternetSpeedTest/tools/PopularWebsiteGetter.java
@@ -3,13 +3,14 @@ package com.github.assisstion.InternetSpeedTest.tools;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import com.github.assisstion.InternetSpeedTest.web.WebConnector;
 
 //Use http://en.wikipedia.org/wiki/List_of_most_popular_websites
 //Sept. 5, 2014
 public class PopularWebsiteGetter{
-	
+
 	public static void main(String[] args){
 		try{
 			getWebsites();
@@ -23,14 +24,21 @@ public class PopularWebsiteGetter{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static String[] getWebsites() throws MalformedURLException, IOException{
+		String name = "";
+		String url = "";
+		boolean thing = false;
+		ArrayList<String> nameA = new ArrayList<String>();
+		ArrayList<String> urlA = new ArrayList<String>();
 		String[] sitesTemp = new String[100];
 		String html = WebConnector.getWebpage(new URL(
 				"http://en.wikipedia.org/wiki/List_of_most_popular_websites"));
 		String[] split = html.split("</tr>\n<tr>");
 		//Accidentally skipped 100th place
 		for(int i = 0; i < split.length; i++){
+			//System.out.println(i + "\n");
+			thing = false;
 			String website = split[i];
 			if(i == 0){
 				continue;
@@ -39,14 +47,41 @@ public class PopularWebsiteGetter{
 				continue;
 			}
 			String[] lines = website.split("\n");
+			
 			for(int j = 0; j < lines.length; j++){
-				String line = lines[j];
-				if(j == 0){
-					continue;
+				//System.out.println(lines[j]);
+				switch(j){
+					case 0:
+						continue;
+					case 1:
+						if(lines[1].contains("\">")){
+							String[] firstLine = lines[1].split("\">")[1].split("</a>");
+							name = firstLine[0];
+							nameA.add(name);
+							thing = true;
+						}
+						break;
+					case 2:
+						if(thing){
+							String[] secondLine = lines[2].split("<td>")[1].split("</td>");
+							url = "http://www." + secondLine[0];
+							urlA.add(url);
+							//System.out.println(url);
+						}
+						break;
 				}
-				System.out.println(line);
 			}
 		}
+		for(int i = 0; i < nameA.size();i++){
+			System.out.println(i);
+			System.out.println(nameA.get(i));
+			System.out.println(urlA.get(i));
+			System.out.println();
+			
+		}
+
+
+
 		return sitesTemp;
 	}
 }
