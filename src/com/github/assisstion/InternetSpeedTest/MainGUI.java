f4091d5552dcd0f71c8facc3779baeea49ff58cb
picrazy2 <alexanderguo99@gmail.com>
2014-09-04 22:00:17 -0400

    GOODBYE!

--- src/com/github/assisstion/InternetSpeedTest/MainGUI.java
+++ src/com/github/assisstion/InternetSpeedTest/MainGUI.java
@@ -2,6 +2,6 @@ package com.github.assisstion.InternetSpeedTest;
 
 public class MainGUI {
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		System.out.println("Goodbye, world!");
 	}
 }
