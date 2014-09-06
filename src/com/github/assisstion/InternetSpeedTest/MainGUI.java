8915431122bad65b54e95c36cd85d11813a3ba05
Alexander Guo <alexanderguo99@gmail.com>
2014-09-06 16:56:33 -0400

    Added settings + help windows; minor font changes

--- src/com/github/assisstion/InternetSpeedTest/MainGUI.java
+++ src/com/github/assisstion/InternetSpeedTest/MainGUI.java
@@ -1,6 +1,7 @@
 package com.github.assisstion.InternetSpeedTest;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -34,6 +35,13 @@ public class MainGUI extends JFrame{
 	private JLabel CRunSpKB;
 	private JButton btnGetGraphs;
 	private GraphWindow window;
+	private HelpWindow helpWindow;
+	private SettingsWindow settingsWindow;
+
+	private JLabel lblTimePassed;
+	private JLabel label_3;
+	private JButton btnAbout;
+	private JButton btnSettings;
 
 	/**
 	 * Launch the application.
@@ -57,111 +65,170 @@ public class MainGUI extends JFrame{
 	 * Create the frame.
 	 */
 	public MainGUI(){
+		setTitle("Internet Speedester");
 		window = new GraphWindow();
+		helpWindow = new HelpWindow();
+		settingsWindow = new SettingsWindow();
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 149);
+		setBounds(100, 100, 450, 173);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JLabel lblSpeed = new JLabel("Website Speed:");
-		lblSpeed.setBounds(21, 53, 101, 16);
+		lblSpeed.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		lblSpeed.setBounds(18, 81, 101, 16);
 		contentPane.add(lblSpeed);
 
 		JLabel lblCumulativeSpeed = new JLabel("Cumulative Speed:");
-		lblCumulativeSpeed.setBounds(19, 71, 116, 16);
+		lblCumulativeSpeed.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		lblCumulativeSpeed.setBounds(16, 99, 116, 16);
 		contentPane.add(lblCumulativeSpeed);
 
 		JLabel lblCurrentWebsite = new JLabel("Current Website:");
-		lblCurrentWebsite.setBounds(19, 36, 116, 16);
+		lblCurrentWebsite.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		lblCurrentWebsite.setBounds(16, 64, 116, 16);
 		contentPane.add(lblCurrentWebsite);
 
 		website = new JLabel("N/A");
-		website.setBounds(172, 35, 61, 16);
+		website.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		website.setBounds(169, 63, 167, 16);
 		contentPane.add(website);
 
 		speed = new JLabel("N/A");
-		speed.setBounds(172, 54, 61, 16);
+		speed.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		speed.setBounds(169, 82, 61, 16);
 		contentPane.add(speed);
 
 		cumulativeSpeed = new JLabel("N/A");
-		cumulativeSpeed.setBounds(172, 72, 61, 16);
+		cumulativeSpeed.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		cumulativeSpeed.setBounds(169, 100, 61, 16);
 		contentPane.add(cumulativeSpeed);
 
 		JLabel lblTime = new JLabel("Time:");
-		lblTime.setBounds(232, 55, 42, 16);
+		lblTime.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		lblTime.setBounds(229, 83, 42, 16);
 		contentPane.add(lblTime);
 
 		JLabel lblKb = new JLabel("KB:");
-		lblKb.setBounds(345, 55, 25, 16);
+		lblKb.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		lblKb.setBounds(342, 83, 25, 16);
 		contentPane.add(lblKb);
 
 		JLabel label = new JLabel("Time:");
-		label.setBounds(233, 72, 42, 16);
+		label.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		label.setBounds(230, 100, 42, 16);
 		contentPane.add(label);
 
 		JLabel label_1 = new JLabel("KB:");
-		label_1.setBounds(345, 72, 25, 16);
+		label_1.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		label_1.setBounds(342, 100, 25, 16);
 		contentPane.add(label_1);
 
 		siteTime = new JLabel("N/A");
-		siteTime.setBounds(278, 55, 61, 16);
+		siteTime.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		siteTime.setBounds(275, 83, 61, 16);
 		contentPane.add(siteTime);
 
 		siteKB = new JLabel("N/A");
-		siteKB.setBounds(383, 55, 61, 16);
+		siteKB.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		siteKB.setBounds(380, 83, 61, 16);
 		contentPane.add(siteKB);
 
 		time = new JLabel("N/A");
-		time.setBounds(278, 72, 61, 16);
+		time.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		time.setBounds(275, 100, 61, 16);
 		contentPane.add(time);
 
 		kb = new JLabel("N/A");
-		kb.setBounds(383, 72, 61, 16);
+		kb.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		kb.setBounds(380, 100, 61, 16);
 		contentPane.add(kb);
 
 		lblCurrentRun = new JLabel("Current Run:");
-		lblCurrentRun.setBounds(19, 19, 116, 16);
+		lblCurrentRun.setFont(new Font("Helvetica Neue", Font.BOLD, 13));
+		lblCurrentRun.setBounds(16, 47, 116, 16);
 		contentPane.add(lblCurrentRun);
 
 		run = new JLabel("N/A");
-		run.setBounds(172, 19, 61, 16);
+		run.setFont(new Font("Helvetica Neue", Font.BOLD, 13));
+		run.setBounds(169, 47, 61, 16);
 		contentPane.add(run);
 
-		lblCumulativeRunSpeed = new JLabel("Cumulative Run Speed");
-		lblCumulativeRunSpeed.setBounds(19, 89, 140, 16);
+		lblCumulativeRunSpeed = new JLabel("Cumulative Run Speed:");
+		lblCumulativeRunSpeed.setFont(new Font("Helvetica Neue", Font.BOLD, 13));
+		lblCumulativeRunSpeed.setBounds(16, 117, 151, 16);
 		contentPane.add(lblCumulativeRunSpeed);
 
 		label_2 = new JLabel("N/A");
-		label_2.setBounds(172, 90, 61, 16);
+		label_2.setFont(new Font("Helvetica Neue", Font.BOLD, 13));
+		label_2.setBounds(169, 118, 61, 16);
 		contentPane.add(label_2);
 
 		lblcRunSpTime = new JLabel("Time:");
-		lblcRunSpTime.setBounds(233, 89, 42, 16);
+		lblcRunSpTime.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		lblcRunSpTime.setBounds(230, 117, 42, 16);
 		contentPane.add(lblcRunSpTime);
 
 		cRunSpTime = new JLabel("N/A");
-		cRunSpTime.setBounds(278, 89, 61, 16);
+		cRunSpTime.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		cRunSpTime.setBounds(275, 117, 61, 16);
 		contentPane.add(cRunSpTime);
 
 		lblCRunSpKB = new JLabel("KB:");
-		lblCRunSpKB.setBounds(345, 89, 25, 16);
+		lblCRunSpKB.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		lblCRunSpKB.setBounds(342, 117, 25, 16);
 		contentPane.add(lblCRunSpKB);
 
 		CRunSpKB = new JLabel("N/A");
-		CRunSpKB.setBounds(383, 89, 61, 16);
+		CRunSpKB.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		CRunSpKB.setBounds(380, 117, 61, 16);
 		contentPane.add(CRunSpKB);
 
 		btnGetGraphs = new JButton("GET GRAPHS");
+		btnGetGraphs.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
 		btnGetGraphs.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				window.setVisible(true);
 			}
 		});
-		btnGetGraphs.setBounds(301, 16, 117, 29);
+		btnGetGraphs.setBounds(283, 10, 131, 29);
 		contentPane.add(btnGetGraphs);
+
+		lblTimePassed = new JLabel("Time Passed:");
+		lblTimePassed.setFont(new Font("Helvetica Neue", Font.BOLD, 13));
+		lblTimePassed.setBounds(229, 47, 116, 16);
+		contentPane.add(lblTimePassed);
+
+		label_3 = new JLabel("N/A");
+		label_3.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		label_3.setBounds(342, 47, 61, 16);
+		contentPane.add(label_3);
+
+		btnAbout = new JButton("?");
+		btnAbout.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		btnAbout.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				helpWindow.setVisible(true);
+			}
+		});
+		btnAbout.setBounds(10, 10, 42, 29);
+		contentPane.add(btnAbout);
+
+		btnSettings = new JButton("Settings");
+		btnSettings.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				settingsWindow.setVisible(true);
+			}
+		});
+		btnSettings.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		btnSettings.setBounds(50, 10, 88, 29);
+		contentPane.add(btnSettings);
 
 		new Thread(new Runnable(){
 
