d237ca56902fce807d66e1d103b6e3ae286bf8a4
Alexander Guo <alexanderguo99@gmail.com>
2014-09-07 00:10:01 -0400

    more stuff on help window

--- src/com/github/assisstion/InternetSpeedTest/HelpWindow.java
+++ src/com/github/assisstion/InternetSpeedTest/HelpWindow.java
@@ -1,6 +1,7 @@
 package com.github.assisstion.InternetSpeedTest;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.SystemColor;
 
 import javax.swing.JFrame;
@@ -44,17 +45,25 @@ public class HelpWindow extends JFrame{
 		contentPane.setLayout(null);
 
 		JLabel lblInternetSpeedtester = new JLabel("Internet Speedtester");
+		lblInternetSpeedtester.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
 		lblInternetSpeedtester.setBounds(80, 50, 132, 16);
 		contentPane.add(lblInternetSpeedtester);
 
 		JLabel lblV = new JLabel("v1.0");
+		lblV.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
 		lblV.setBounds(125, 75, 34, 16);
 		contentPane.add(lblV);
 
 		JTextPane txtpnHi = new JTextPane();
+		txtpnHi.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
 		txtpnHi.setBackground(SystemColor.window);
-		txtpnHi.setText("This is a simple program that tests your wifi ");
-		txtpnHi.setBounds(33, 116, 111, 16);
+		txtpnHi.setText("This is a simple program that tests your wifi speed by accessing the homepages of some of the most commonly accesse websites with servers in the US. You may change the list in the settings window.");
+		txtpnHi.setBounds(33, 123, 219, 130);
 		contentPane.add(txtpnHi);
+
+		JLabel lblMarkusFeng = new JLabel("© Markus Feng and Alex Guo");
+		lblMarkusFeng.setFont(new Font("Helvetica Neue", Font.PLAIN, 9));
+		lblMarkusFeng.setBounds(79, 98, 188, 16);
+		contentPane.add(lblMarkusFeng);
 	}
 }
