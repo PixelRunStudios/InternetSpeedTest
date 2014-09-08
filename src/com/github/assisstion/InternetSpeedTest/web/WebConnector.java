c99b290f59f5a9411a97cc32d9e6d96edb878ef9
Markus Feng <zzz1112222595@hotmail.com>
2014-09-07 21:50:40 -0400

    Added timeout feature to website loading
    Default timeout: 5 seconds; loading is slightly slower

--- src/com/github/assisstion/InternetSpeedTest/web/WebConnector.java
+++ src/com/github/assisstion/InternetSpeedTest/web/WebConnector.java
@@ -80,30 +80,35 @@ public class WebConnector {
 
 		@Override
 		protected Pair<Long, Long> doInBackground() throws Exception{
-			//Gets the input stream to load the website
-			InputStream is = url.openConnection().getInputStream();
+			//Gets the input stream to load the website: timeout 5 seconds
+			InputStream is = loadWithTimeOut(url, 5000);
 			//Wraps the input stream with a buffer
 			BufferedInputStream bis = new BufferedInputStream(is);
 			//Starts the timer
 			startTime = System.currentTimeMillis();
-			//Counts one unicode byte
-			int value = bis.read();
+			//Counts one unicode byte: timeout 5 seconds
+			//int value = bis.read();
+			int value = readWithTimeOut(bis, 5000);
 			count = 0;
 			while(value >= 0){
 				count++;
 				if(count % 1000 == 0){
 					publish(new Pair<Long, Long>(count, System.currentTimeMillis()));
-					if(count % 1000000 == 0){
-						if(!silent){
-							System.out.println("Counted " + count / 1000000 + " million.");
-						}
-						if(count >= 100000000){
-							break;
-						}
+				}
+				if(count % 1000000 == 0){
+					value = readWithTimeOut(bis, 5000);
+					if(!silent){
+						System.out.println("Counted " + count / 1000000 + " million.");
+					}
+					if(count >= 100000000){
+						break;
 					}
 				}
-				//Sets the value to the next character
-				value = bis.read();
+				else{
+					//Sets the value to the next character
+					value = bis.read();
+					//value = readWithTimeOut(bis, 1000);
+				}
 			}
 			long difference = System.currentTimeMillis() - startTime;
 			return new Pair<Long, Long>(count, difference);
@@ -126,8 +131,140 @@ public class WebConnector {
 
 		}
 
+	}
 
+	/**
+	 * Read a byte of data from the input stream with a certain time limit
+	 * @param is
+	 * @param timeOut
+	 * @return
+	 */
+	public static int readWithTimeOut(InputStream is, long timeOut) throws IOException{
+		TimedReader reader = new TimedReader(is);
+		TimeOutRunner<Integer> runner = new TimeOutRunner<Integer>(reader, timeOut);
+		reader.setRunner(runner);
+		return runner.get();
+	}
 
+	public static InputStream loadWithTimeOut(URL url, long timeOut) throws IOException{
+		TimedURLLoader reader = new TimedURLLoader(url);
+		TimeOutRunner<InputStream> runner = new TimeOutRunner<InputStream>(reader, timeOut);
+		reader.setRunner(runner);
+		return runner.get();
+	}
+
+	public static class TimeOutRunner<T>{
+		protected T value;
+		protected Runnable runnable;
+		protected boolean runnableStarted;
+
+		protected long timeOut;
+
+		protected Exception toBeThrown;
+
+		public TimeOutRunner(Runnable runnable, long timeOut){
+			this.runnable = runnable;
+			this.timeOut = timeOut;
+		}
+
+		public T get() throws IOException{
+			value = null;
+			toBeThrown = null;
+			runnableStarted = false;
+			long startTime = System.currentTimeMillis();
+			while(System.currentTimeMillis() - startTime < timeOut){
+				synchronized(this){
+					if(!runnableStarted){
+						runnableStarted = true;
+						new Thread(runnable).start();
+					}
+					try{
+						wait(timeOut);
+					}
+					catch(InterruptedException e){
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if(toBeThrown != null){
+						throw new IOException(
+								"Exception caught while loading", toBeThrown);
+					}
+					if(value != null){
+						return value;
+					}
+				}
+			}
+			System.out.println("TO!");
+			throw new IOException("Loading timeout");
+		}
+
+		public void putValue(T value){
+			this.value = value;
+		}
+
+		public void throwException(Exception e){
+			toBeThrown = e;
+			synchronized(this){
+				notifyAll();
+			}
+		}
+	}
+
+	public static class TimedReader implements Runnable{
+
+		protected TimeOutRunner<Integer> runner;
+
+		protected InputStream is;
+
+		public TimedReader(InputStream is){
+			this.is = is;
+		}
+
+		public void setRunner(TimeOutRunner<Integer> runner){
+			this.runner = runner;
+		}
+
+		@Override
+		public void run(){
+			try{
+				runner.putValue(is.read());
+			}
+			catch(Exception e){
+				runner.throwException(e);
+			}
+			synchronized(runner){
+				runner.notifyAll();
+			}
+		}
+
+	}
+
+	public static class TimedURLLoader implements Runnable{
+
+		protected URL url;
+
+		protected TimeOutRunner<InputStream> runner;
+
+		public TimedURLLoader(URL url){
+			this.url = url;
+		}
+
+		public void setRunner(TimeOutRunner<InputStream> runner){
+			this.runner = runner;
+		}
+
+		@Override
+		public void run(){
+			try{
+				runner.putValue(url.openConnection().getInputStream());
+			}
+			catch(Exception e){
+				runner.throwException(e);
+			}
+			synchronized(runner){
+				runner.notifyAll();
+			}
+		}
 
 	}
 
