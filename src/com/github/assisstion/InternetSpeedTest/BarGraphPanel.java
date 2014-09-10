dc71aa2c2ba9dcd06abe1a1a5474b705172ca69d
Alexander Guo <alexanderguo99@gmail.com>
2014-09-09 22:22:06 -0400

    Added basic graph on BarGraphWindow

--- src/com/github/assisstion/InternetSpeedTest/BarGraphPanel.java
+++ src/com/github/assisstion/InternetSpeedTest/BarGraphPanel.java
@@ -1,5 +1,8 @@
 package com.github.assisstion.InternetSpeedTest;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class BarGraphPanel extends JPanel{
@@ -8,6 +11,9 @@ public class BarGraphPanel extends JPanel{
 	 *
 	 */
 	private static final long serialVersionUID = -509125879716516235L;
+	public static String site1;
+	public static long bytes1;
+	public static long loadingTime1;
 
 
 	/**
@@ -15,10 +21,23 @@ public class BarGraphPanel extends JPanel{
 	 */
 	public BarGraphPanel(){
 
+
+	}
+	@Override
+	public void paint(Graphics g) {
+		g.setColor(Color.blue);
+		g.fillRect(50, 30, 200, 380);
+		g.setColor(Color.black);
+		g.drawLine(50, 390, 700, 390);
+		g.drawLine(50, 30, 50, 390);
+
 	}
 
 
 	public void pushBar(String site, long bytes, long loadingTime){
-
+		site1 =site;
+		bytes1 = bytes;
+		loadingTime1 = loadingTime;
+		repaint();
 	}
 }
--- src/com/github/assisstion/InternetSpeedTest/GraphWindow.java
+++ src/com/github/assisstion/InternetSpeedTest/GraphWindow.java
@@ -1,6 +1,7 @@
 package com.github.assisstion.InternetSpeedTest;
 
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,7 +24,7 @@ public class GraphWindow extends JFrame{
 	 */
 	public GraphWindow(){
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 800, 300);
+		setBounds(100, 100, 1000, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -34,6 +35,21 @@ public class GraphWindow extends JFrame{
 		tabbedPane.addTab("By Website", sitePanel);
 
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
+	}
+
+	public static void main(String[] args){
+		EventQueue.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				try{
+					GraphWindow frame = new GraphWindow();
+					frame.setVisible(true);
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
