1688eb59d0be87306323cbcbea729c97d9898685
Markus Feng <markusfeng@gmail.com>
2014-09-10 19:51:05 -0400

    Made changes to BarGraphPanel
    Y axis numbers not really working

--- src/com/github/assisstion/InternetSpeedTest/BarGraphPanel.java
+++ src/com/github/assisstion/InternetSpeedTest/BarGraphPanel.java
@@ -19,23 +19,21 @@ public class BarGraphPanel extends JPanel{
 	 *
 	 */
 	private static final long serialVersionUID = -509125879716516235L;
-	public int xElements = 0;
-	public int currentXElement = 0;
-	public int numberRuns = 1;
-	public int siteP = 0;
-	public int numSites = 0;
+	private int xElements = 0;
+	private int currentXElement = 0;
+	private int siteP = 0;
 
-	public static final int TOP_Y = 30;
-	public static final int BOTTOM_Y = 390;
-	public static final int LEFT_X = 50;
-	public static final int RIGHT_X = 920;
+	private static final int TOP_Y = 30;
+	private static final int BOTTOM_Y = 390;
+	private static final int LEFT_X = 50;
+	private static final int RIGHT_X = 920;
 
 	private static final int MARKER_LENGTH = 10;
 	private static final int DIST_FROM_MARKER = 2;
 
 	//private int min = 0;
 	private long max = 0;
-	private long cap = 200;
+	private static final int CAP = 200;
 	int markerAmount = 0;
 
 
@@ -55,33 +53,20 @@ public class BarGraphPanel extends JPanel{
 	}
 	@Override
 	public void paint(Graphics g) {
-		g.setColor(Color.blue);
-		//g.fillRect(50, 30, 200, 380);
-		g.setColor(Color.black);
+		g.setColor(Color.BLACK);
 		g.drawRect(LEFT_X, TOP_Y, RIGHT_X-LEFT_X, BOTTOM_Y-TOP_Y);
-		for(int i = 0; i<xElements-1;i++){
-			g.setColor(Color.black);
-			g.drawLine(LEFT_X+siteP*(i+1),BOTTOM_Y, LEFT_X+siteP*(i+1), BOTTOM_Y+MARKER_LENGTH);
+		for(int i = 0; i < xElements - 1;i++){
+			int x = LEFT_X + siteP * (i+1);
+			g.drawLine(x, BOTTOM_Y, x, BOTTOM_Y+MARKER_LENGTH);
 
 		}
 		int i = 0;
 		for(Map.Entry<String, Pair<Long, Long>> entry : speeds.entrySet()){
-			g.setColor(Color.blue);
-
-			//System.out.println("hi1: "+ speeds.get(i));
-			//System.out.println("hi: "+(int)((BOTTOM_Y-TOP_Y)/((max+cap)*1.0/speeds.get(i))));
-			//System.out.println("max = "+max);
 			Graphics2D g2d = (Graphics2D) g;
-			//AffineTransform form = new AffineTransform();
-			//form.rotate(-Math.PI / 2);
-			//Font font = g2d.getFont();
-			//Font x = font.deriveFont(form);
-			//g2d.setFont(x);
 			g2d.setColor(Color.BLACK);
 			AffineTransform form = new AffineTransform();
 			form.rotate(-Math.PI / 2, getWidth()/2, getHeight()/2);
 			g2d.transform(form);
-			//g2d.drawString(entry.getKey(), LEFT_X+siteP*i+DIST_FROM_MARKER, BOTTOM_Y+20);
 			g2d.drawString(entry.getKey(), BOTTOM_Y+20-150, LEFT_X+siteP*i+DIST_FROM_MARKER - 205);
 			try{
 				g2d.transform(form.createInverse());
@@ -91,61 +76,67 @@ public class BarGraphPanel extends JPanel{
 				e.printStackTrace();
 			}
 			g2d.setColor(Color.BLUE);
-			//g2d.setFont(font);
 			if(entry.getValue().getValueOne() != 0 && entry.getValue().getValueTwo() != 0){
 				long speedNow = entry.getValue().getValueOne() / entry.getValue().getValueTwo();
 
-				g.fillRect(LEFT_X+siteP*i+DIST_FROM_MARKER, BOTTOM_Y - (int)((BOTTOM_Y-TOP_Y)/((double)(max+cap)/speedNow)), siteP-2*DIST_FROM_MARKER, (int)((BOTTOM_Y-TOP_Y)/((double)(max+cap)/speedNow)));
+				g.fillRect(LEFT_X+siteP*i+DIST_FROM_MARKER, BOTTOM_Y - (int)((BOTTOM_Y-TOP_Y)/((double)(max+CAP)/speedNow)), siteP-2*DIST_FROM_MARKER, (int)((BOTTOM_Y-TOP_Y)/((double)(max+CAP)/speedNow)));
 			}
 			i++;
 		}
-		if(max+cap >0 && max+ cap <=25){
+		markerAmount = (int)(max+CAP) / 50 * 5;
+		/*
+		if(max+CAP >0 && max+ CAP <=25){
 			markerAmount = 5;
 		}
-		else if(max+cap >25 && max+ cap <=100){
+		else if(max+CAP >25 && max+ CAP <=100){
 			markerAmount = 5;
 		}
-		else if(max+cap >100 && max+ cap <=250){
+		else if(max+CAP >100 && max+ CAP <=250){
 			markerAmount = 25;
 		}
-		else if(max+cap >250 && max+ cap <=500){
+		else if(max+CAP >250 && max+ CAP <=500){
 			markerAmount = 50;
 		}
-		else if(max+cap >500 && max+ cap <=1000){
+		else if(max+CAP >500 && max+ CAP <=1000){
 			markerAmount = 100;
 		}
-		else if(max+cap >1000 && max+ cap <=2000){
+		else if(max+CAP >1000 && max+ CAP <=2000){
 			markerAmount = 200;
 		}
-		else if(max+cap >2000 && max+ cap <=5000){
+		else if(max+CAP >2000 && max+ CAP <=5000){
 			markerAmount = 500;
 		}
-		else if(max+cap >5000 && max+ cap <=10000){
+		else if(max+CAP >5000 && max+ CAP <=10000){
 			markerAmount = 1000;
 		}
-		else if(max+cap >10000){
+		else if(max+CAP >10000){
 			markerAmount = 2000;
 		}
-		g.setColor(Color.black);
+		 */
+		g.setColor(Color.BLACK);
 
 		int counter = 0;
-		for(int j = 0; j<max+cap+1; j+=markerAmount){
-			//System.out.println("mc = " + (max + cap));
-			//System.out.println("j = " + j);
-			g.drawLine(LEFT_X, (int) (TOP_Y + j*((BOTTOM_Y-TOP_Y)/((max+cap)/markerAmount))), LEFT_X + MARKER_LENGTH, (int)(TOP_Y + j*((BOTTOM_Y-TOP_Y)/((max+cap)/markerAmount))));
-			g.drawString(""+ (max+cap-j), LEFT_X-25, (int) (TOP_Y + counter++*((BOTTOM_Y-TOP_Y)/((double)(max+cap)/markerAmount))));
+		/*
+		for(int j = 0; j<max+CAP+1; j+=markerAmount){
+			g.drawLine(LEFT_X, (int) (TOP_Y + j*((BOTTOM_Y-TOP_Y)/((max+CAP)/markerAmount))), LEFT_X + MARKER_LENGTH, (int)(TOP_Y + j*((BOTTOM_Y-TOP_Y)/((max+CAP)/markerAmount))));
+			g.drawString(""+ (max+CAP-j), LEFT_X-25, (int) (TOP_Y + counter++*((BOTTOM_Y-TOP_Y)/((double)(max+CAP)/markerAmount))));
 		}
+		 */
+
+		for(int j = (int)(max+CAP) % markerAmount; j <= max+CAP; j+=markerAmount){
+			g.drawLine(LEFT_X, (int) (TOP_Y + j*((BOTTOM_Y-TOP_Y)/((max+CAP)/markerAmount))), LEFT_X + MARKER_LENGTH, (int)(TOP_Y + j*((BOTTOM_Y-TOP_Y)/((max+CAP)/markerAmount))));
+			g.drawString(String.valueOf(max+CAP-j), LEFT_X-32, (int) (TOP_Y + counter++*((BOTTOM_Y-TOP_Y)/((double)(max+CAP)/markerAmount))) + (int)((max+CAP) % markerAmount/(double)(max+CAP)*(BOTTOM_Y-TOP_Y)));
+		}
+		g.drawString(String.valueOf(max+CAP), LEFT_X+2, (int) (TOP_Y + (BOTTOM_Y-TOP_Y)/((double)(max+CAP)/markerAmount)) + (int)((max+CAP) % markerAmount/(double)(max+CAP)*(BOTTOM_Y-TOP_Y)) - (int)(markerAmount/2.0/(max+CAP)*(BOTTOM_Y-TOP_Y)));
 	}
 
 
 	public void pushBar(String site, long bytes, long loadingTime, int sites){
-		//System.out.println(speeds.size());
 
-		if(xElements<sites){
+		if(xElements < sites){
 			xElements++;
 		}
 		if(speeds.get(site) != null){
-			//System.out.println("counter2: " + currentXElement);
 			Pair<Long, Long> pair = speeds.get(site);
 			speeds.put(site, new Pair<Long, Long>(
 					pair.getValueOne() + bytes, pair.getValueTwo() + loadingTime));
@@ -154,12 +145,8 @@ public class BarGraphPanel extends JPanel{
 			speeds.put(site, new Pair<Long, Long>(
 					bytes, loadingTime));
 		}
-		//System.out.println(sites);
-		//System.out.println(currentXElement);
-		//System.out.println(speeds.size());
-		numSites = sites;
-		siteP = (RIGHT_X-LEFT_X)/xElements;
-		max=0;
+		siteP = (RIGHT_X-LEFT_X) / xElements;
+		max = 0;
 		for(Map.Entry<String, Pair<Long, Long>> entry : speeds.entrySet()){
 			Pair<Long, Long> pair = entry.getValue();
 			if(pair.getValueTwo() == 0){
@@ -171,11 +158,10 @@ public class BarGraphPanel extends JPanel{
 			}
 		}
 		repaint();
-		if(currentXElement<sites-1){
+		if(currentXElement < sites - 1){
 			currentXElement++;
 		}
-		else if(currentXElement ==sites-1){
-			numberRuns++;
+		else if(currentXElement == sites - 1){
 			currentXElement = 0;
 		}
 		else{
