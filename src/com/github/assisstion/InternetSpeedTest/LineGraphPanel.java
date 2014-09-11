19e3fb901dd29d2d133a76b4d857ac1cc24083a6
Markus Feng <zzz1112222595@hotmail.com>
2014-09-11 19:38:52 -0400

    Made a working LineGraphPanel
    Line only

--- src/com/github/assisstion/InternetSpeedTest/LineGraphPanel.java
+++ src/com/github/assisstion/InternetSpeedTest/LineGraphPanel.java
@@ -2,10 +2,9 @@ package com.github.assisstion.InternetSpeedTest;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 import javax.swing.JPanel;
 
@@ -21,12 +20,11 @@ public class LineGraphPanel extends JPanel{
 	private static final int LEFT_X = 50;
 	private static final int RIGHT_X = 920;
 
-	private TreeMap<Long, Double> data = new TreeMap<Long, Double>();
-	private long timeDifference = getGraphWidth() / 10;
-	private long leftTime = 0;
-	private int pixelInterval = 2;
 
-	private TreeSet<Line> lines = new TreeSet<Line>();
+	private TreeMap<Long, Double> data = new TreeMap<Long, Double>();
+	private int valuesPerPoint = 1;
+	private int pixelInterval = 2;
+	private int max = 0;
 
 	/**
 	 * Create the panel.
@@ -39,18 +37,50 @@ public class LineGraphPanel extends JPanel{
 	public void paint(Graphics g){
 		g.setColor(Color.BLACK);
 		g.drawRect(LEFT_X, TOP_Y, getGraphWidth(), getGraphHeight());
-		Graphics2D g2d = (Graphics2D) g;
-		for(Map.Entry<Long, Double> entry : data.entrySet()){
-			//TODO
+		int size = getGraphWidth() / pixelInterval;
+		ArrayList<Double> points = new ArrayList<Double>(size / valuesPerPoint);
+		Map.Entry<Long, Double> entry = data.lastEntry();
+		int speedStack = 0;
+		int speedStackCount = 0;
+		for(int i = 0; i < size; i++){
+			if(entry == null){
+				break;
+			}
+			speedStack += entry.getValue();
+			speedStackCount++;
+			if(speedStackCount >= valuesPerPoint){
+				double speed = (double) speedStack / (double) speedStackCount;
+				points.add(speed);
+				if(speed > max){
+					max = (int) speed;
+				}
+				speedStack = 0;
+				speedStackCount = 0;
+			}
+			entry = data.lowerEntry(entry.getKey());
 		}
-		for(Line line : lines){
-			//TODO
+		boolean hasLast = false;
+		int lastX = LEFT_X;
+		int lastY = BOTTOM_Y;
+		for(int i = 0; i < points.size(); i++){
+			double value = points.get(points.size() - i - 1);
+			int y = BOTTOM_Y;
+			if(hasLast){
+				y = BOTTOM_Y - (int) (value * getGraphHeight() / max);
+				System.out.println("X:" + (lastX + 2));
+				System.out.println(y);
+				g.drawLine(lastX, lastY, lastX + 2, y);
+			}
+			lastX = lastX + 2;
+			lastY = y;
+			hasLast = true;
 		}
 	}
 
 
 	public void pushLine(long timeStamp, double speed){
 		data.put(timeStamp, speed);
+		repaint();
 	}
 
 	public static int getGraphWidth(){
@@ -61,16 +91,14 @@ public class LineGraphPanel extends JPanel{
 		return BOTTOM_Y - TOP_Y;
 	}
 
-	public static class Line implements Comparable<Line>{
+	public static class Point implements Comparable<Point>{
 
-		public int x1;
-		public int y1;
-		public int x2;
-		public int y2;
+		public int x;
+		public int y;
 
 		@Override
-		public int compareTo(Line o){
-			return Integer.compare(x1, o.x1);
+		public int compareTo(Point o){
+			return Integer.compare(x, o.x);
 		}
 	}
 }
